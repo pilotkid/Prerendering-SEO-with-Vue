ad8cc5bc584f77082e2cf97e75bdc23cd096dd53
Marcello A. Bachechi <pilotkid2011@gmail.com>
2022-01-31 16:05:10 -0700

    Configure plugin

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,27 @@
-module.exports = {
-  transpileDependencies: [
-    'vuetify'
-  ]
+const plugins = [];
+
+if (process.env.NODE_ENV === 'production') {
+	const { join } = require('path');
+	const PrerenderPlugin = require('prerender-spa-plugin');
+	const renderer = PrerenderPlugin.PuppeteerRenderer;
+
+	plugins.unshift(
+		new PrerenderPlugin({
+			staticDir: join(__dirname, 'dist'),
+			routes: ['/dynamic', '/'],
+			renderer: new renderer({
+				renderAfterDocumentEvent: 'render-complete',
+				headless: false,
+				// renderAfterTime: 5 * 60 * 60 * 1000,
+				maxConcurrentRoutes: 1,
+			}),
+		})
+	);
 }
+
+module.exports = {
+	transpileDependencies: ['vuetify'],
+	configureWebpack(config) {
+		config.plugins = [...config.plugins, ...plugins];
+	},
+};
