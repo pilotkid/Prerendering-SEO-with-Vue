d8a7af67a76125be18117712bcc29fa9ed838d0b
Marcello A. Bachechi <pilotkid2011@gmail.com>
2022-01-31 16:38:40 -0700

    Created display of data

--- src/views/noIndex.vue
+++ src/views/noIndex.vue
@@ -5,19 +5,26 @@
 		</center>
 		<v-divider class="my-4" />
 		<v-slider v-model="sliderValue" :min="1" :max="10" />
-		<div v-for="(i, idx) in (1, 10)" :key="i">
-			<v-card class="mb-2" v-if="i == sliderValue">
-				<v-card-title> Element {{ i }} showing </v-card-title>
-				<v-card-text>
-					<h3>
-						{{ randomWordsSequence[idx] }}
-					</h3>
-				</v-card-text>
-				<v-card-actions>
-					<v-spacer />
-					<v-btn text @click="sliderValue++"> Next </v-btn>
-				</v-card-actions>
-			</v-card>
+		<div v-if="isPrerendering">
+			<p v-for="i in randomWordsSequence" :key="i">
+				{{ i }}
+			</p>
+		</div>
+		<div v-else>
+			<div v-for="(i, idx) in (1, 10)" :key="i">
+				<v-card class="mb-2" v-if="i == sliderValue">
+					<v-card-title> Element {{ i }} showing </v-card-title>
+					<v-card-text>
+						<h3>
+							{{ randomWordsSequence[idx] }}
+						</h3>
+					</v-card-text>
+					<v-card-actions>
+						<v-spacer />
+						<v-btn text @click="sliderValue++"> Next </v-btn>
+					</v-card-actions>
+				</v-card>
+			</div>
 		</div>
 	</div>
 </template>
@@ -29,8 +36,18 @@ export default {
 			randomWordsSequence: [],
 		};
 	},
+	computed: {
+		isPrerendering() {
+			return window && window.isPrerendering;
+		},
+	},
 	created() {
 		this.randomWordsSequence = require('@/assets/baconlorem.json');
+	},
+	mounted() {
+		this.$nextTick(() => {
+			document.dispatchEvent(new Event('render-complete'));
+		});
 	},
 };
 </script>
